5f52ac6d0a8515c87a2c9d27e9d1f014e8684b6a
Andrea Del Torchio <andreadeltorchio.m@gmail.com>
2022-03-29 10:10:41 +0200

    Use vector of points for regression

--- regression.hpp
+++ regression.hpp
@@ -1,16 +1,17 @@
 #ifndef REGRESSION_HPP
 #define REGRESSION_HPP
+#include <vector>
 struct Result {
   double A;
   double B;
 };
 
 class Regression {
-  int N_{};
-  double sum_x_{};
-  double sum_y_{};
-  double sum_xy_{};
-  double sum_x2_{};
+  struct Point {
+    double x;
+    double y;
+  };
+  std::vector<Point> points_{};
 
  public:
   int size() const;
